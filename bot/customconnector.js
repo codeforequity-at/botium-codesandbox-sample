6fd16559b2fb66ae97d7df44ad1ed1cbf69974a0
treml <ftreml@gmail.com>
2018-10-29 23:03:08 +0100

    updated botium-cli

--- bot/customconnector.js
+++ bot/customconnector.js
@@ -1,4 +1,4 @@
-const debug = require('debug')('botium-connector-sampelbot')
+const debug = require('debug')('botium-connector-samplebot')
 
 class BotiumConnectorEcho {
   constructor ({ queueBotSays }) {
